ba44c975f68b199ac39876d7e1b4ce0295d6c5c0
Jesus Marin <jesusangelm@gmail.com>
2012-05-24 18:07:57 -0430

    El Genesis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notas::Application.config.secret_token = '7ba667ee5d12cb5dffe5ae044a7abd9b0825a2264f0945711132c334d3f8d5931ba902bfd62a13af094608a3b4303b9fcca956b9c64ad2f514bbcef92df7dbd4'
